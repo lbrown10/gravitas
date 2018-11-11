6c907e9e54ae42fd6d04a68ea8a9faab55d48010
nguyen-amanda <amanda@amandanguyen.com>
2018-11-10 19:06:11 -0600

    Properly made it so that a user can continue where they left off after they finish the game and start over, and additional refactoring

--- gravitas/main.js
+++ gravitas/main.js
@@ -10,29 +10,28 @@ $(function() {
     let gameState = new Game(game, optionsData);
 
     let menu = new Menu(game, optionsData, function() {
-        let levelList = game.cache.getText('levelList').split('\n');
         let playerDataList = localStorage.getItem('user_progress');
 
-        if (playerDataList == null) {
-          playerDataList = [0];
-          for (let i = 1; i < levelList.length; i++) {
-              playerDataList[i] = 1;
-          }
-          localStorage.setItem('user_progress', playerDataList);
+        // Creates 'user_progress' in localStorage.
+        if (playerDataList == null) { // User first plays game
+          resetUserProgress();
           gameState.setLevel(0);
           game.state.start('game');
         } else {
+          // Properly sets current game level.
           playerDataList = playerDataList.split(',');
-          for (let i = 0; i < levelList.length; i++) {
-            if (playerDataList[i] == 1) {
-              gameState.setLevel(i - 1);
-              game.state.start('game');
-              break;
-            }
-            if (i == levelList.length - 1) {
-              gameState.setLevel(0);
-              localStorage.setItem('death_count', 0);
-              game.state.start('game');
+          if (playerDataList.indexOf(1) === -1) { // User has finished the game
+            resetUserProgress();
+            gameState.setLevel(0);
+            localStorage.setItem('death_count', deathCount);
+            game.state.start('game');
+          } else { // User is continuing game
+            for (let i = 0; i < levelList.length; i++) {
+              if (playerDataList[i] == 1) {
+                gameState.setLevel(i - 1);
+                game.state.start('game');
+                break;
+              }
             }
           }
         }
@@ -43,6 +42,15 @@ $(function() {
     let win = new Win(game, optionsData);
 
     let levelSelect = new LevelSelect(game, gameState);
+
+    function resetUserProgress() {
+      let levelList = game.cache.getText('levelList').split('\n');
+      let playerDataList = [0];
+      for (let i = 1; i < levelList.length; i++) {
+          playerDataList[i] = 1;
+      }
+      localStorage.setItem('user_progress', playerDataList);
+    }
 
     game.state.add('boot', {preload: bootState.boot, create: bootState.postBoot});
     game.state.add('menu', {preload: menu.loadMenu, create: menu.createMenu});

ae69e90b551161ecc90c15bbbdec02b9f9a75707
Luke <lbrown10@macalester.edu>
2018-11-12 18:20:25 -0600

    Tiny Bugfix on Playgame Button
    Made the change to start deathcount in local storage at "0". Game was crashing upon hitting the playbutton before I did this.

--- gravitas/main.js
+++ gravitas/main.js
@@ -23,7 +23,7 @@ $(function() {
           if (playerDataList.indexOf(1) === -1) { // User has finished the game
             resetUserProgress();
             gameState.setLevel(0);
-            localStorage.setItem('death_count', deathCount);
+            localStorage.setItem('death_count', 0);
             game.state.start('game');
           } else { // User is continuing game
             for (let i = 0; i < levelList.length; i++) {
